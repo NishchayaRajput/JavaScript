35b8ce795345b40f350032347e0c79beb85a8042
Nishchaya Rajput <114467750+NishchayaRajput@users.noreply.github.com>
2023-11-07 17:00:23 +0000

    config done

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,52 @@
+let quantity = "3442"
+let supply = 1000
+// console.log(typeof (quantity))
+
+//you can convert variables to another variable like this
+let convertInNumber = Number(quantity)
+let convertInString = String(supply)
+// console.log(typeof (convertInNumber))
+// console.log(typeof (convertInString))
+
+//"323" = 323
+//"323abc" = Nan     not a number
+//"null" = 0
+//"undefined" = NaN
+//true = 1,false = 0
+
+
+//convert in boolean 
+let log = 1
+let convertinboolean = Boolean(log);
+// console.log(convertinboolean);
+
+//1 = true
+//0 = false
+//"" = false
+//"harry" = true
+
+//Note = when you convert numbers into string it prints the number but convert into a string
+let num = 43
+let stringnum = String(num)
+// console.log(stringnum);
+// console.log(typeof stringnum);
+
+/* ******************* OPERATIONS ******************** */
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+// Basic Operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = 3
+let str2 = 3
+let str3 = str1 + str2
+console.log(str3);
+
+
---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,17 @@
+let name = "Nishchaya"
+let age = 19
+let logedIn = true;
+
+/*Data types
+numbers - 2 power of 53
+bigint
+string - ""
+boolean - true/false   conditions
+null - standalone value
+undefined 
+symbol - unique
+*/
+
+// object
+// To know the datatype of a object use
+console.log(typeof age);
